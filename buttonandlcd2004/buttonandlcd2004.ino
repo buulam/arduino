b7cca4d63f2ca9b2dcf3f88a1c80a6ccb03dbb1a
blam5 <b.lam@f5.com>
2025-02-08 15:32:32 -0800

    button demos

---
+++ buttonandlcd2004/buttonandlcd2004.ino
@@ -0,0 +1,77 @@
+/*
+  Button
+
+  Turns on and off a light emitting diode(LED) connected to digital pin 13,
+  when pressing a pushbutton attached to pin 2.
+
+  The circuit:
+  - LED attached from pin 13 to ground through 220 ohm resistor
+  - pushbutton attached to pin 2 from +5V
+  - 10K resistor attached to pin 2 from ground
+
+  - Note: on most Arduinos there is already an LED on the board
+    attached to pin 13.
+
+  created 2005
+  by DojoDave <http://www.0j0.org>
+  modified 30 Aug 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Button
+*/
+
+#include <Wire.h>
+#include <hd44780.h>                       // main hd44780 header
+#include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
+
+hd44780_I2Cexp lcd; // declare lcd object: auto locate & config exapander chip
+
+// LCD geometry
+const int LCD_COLS = 20;
+const int LCD_ROWS = 4;
+
+int led = 12;
+int led2 = 4;
+int led3 = 3;
+int brightness = 250;  // how bright the LED is
+int fadeAmount = 5;  // how many points to fade the LED by
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;  // the number of the pushbutton pin
+const int ledPin = 13;    // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;  // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  analogWrite(led, 0);
+  analogWrite(led2, 0);
+  analogWrite(led3, 0);
+  lcd.begin(LCD_COLS, LCD_ROWS);
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    lcd.print("Button On");
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+        lcd.print("Button Off");
+  }
+}
